e472b127ca771370cf2f5e536e6a0bba8071a6aa
gui-source <zcabgct@ucl.ac.uk>
2024-02-28 10:02:09 +0000

    Added script to analyse and plot experiment results

--- ns-3.40/scratch/two-flow.cc
+++ ns-3.40/scratch/two-flow.cc
@@ -13,9 +13,13 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/traffic-control-module.h"
+#include "ns3/rng-seed-manager.h"
 
 using namespace ns3;
 // using Random = effolkronium::random_static;
+
+// Random Seed
+uint32_t seed = 1;
 
 // ---------------------------------------------------- //
 // --- BEGIN OF SIMULATION CONFIGURATION PARAMETERS --- //
@@ -33,30 +37,22 @@ std::string tracesPath = "traces/";
 Time startTimeUDP = Seconds(0.1);
 
 // Time to stop the UDP connection
-Time stopTimeUDP = Seconds(5);
+Time stopTimeUDP = Seconds(100);
 
 // Simulation stop time
-Time stopTimeSimulation = Seconds(60);
-
-// UDP flow interval
-Time intervalUDP = Seconds(1);
+Time stopTimeSimulation = Seconds(101);
 
 // ---[UDP PARAMETERS] ---
-uint32_t segmentSize = 512;
-uint32_t MTU_bytes = segmentSize + 54;
-uint8_t delAckCount = 2;
-uint8_t initialCwnd = 4;
-std::string delAckTimeout = "200ms";
-std::string socketFactory = "ns3::TcpSocketFactory";
-std::string qdiscTypeId = "ns3::PFifoFastQueueDisc";
-double minRTO = 1.0;
+std::string flowDataRateStr = "10Gbps";
+uint32_t maxPacketSize = 1024;
 
 // ---[TOPOLOGY PARAMETERS]---
-std::string bandwidth_bottleneck = "10Mbps";
-std::string bandwidth_access = "20Mbps";
+std::string bandwidth_access = "10Gbps";
 std::string delay_access = "5ms";
-std::string delay_serialization = "1.9ms";
+std::string delay_serialization = "0ms";
 std::string interframe_gap = "0ms";
+double lambda_1 = 1000.00;
+double lambda_2 = 1000.00;
 
 // ---[POINTER TO THE DEVICE THAT WILL IMPLEMENT PACKET DROPING]
 NetDeviceContainer * netDeviceToDropPacket = NULL;
@@ -66,91 +62,46 @@ NetDeviceContainer * netDeviceToDropPacket = NULL;
 // --- END OF SIMULATION CONFIGURATION PARAMETERS --- //
 // -------------------------------------------------- //
 
-// Global Variables
-Ptr<PacketSink> sinker;
+/*
+    lambda should be the expected number of packets per second in the Poisson process
+*/
+void InstallSourceSink(Ptr<Node> src, Ipv4Address destAddress, double lambda){
+    // Set up On time (just time for 1 packet)
+    Ptr<ConstantRandomVariable> onTime = CreateObject<ConstantRandomVariable> ();
+    DataRate dataRate(flowDataRateStr);
+    double onRate = ((double) maxPacketSize) * 8.0 / dataRate.GetBitRate();
+    onTime->SetAttribute("Constant", DoubleValue(onRate));
 
-int packetsDroppedInQueue = 0;
-int64_t lastTotalRx = 0;
-uint32_t bytes_to_send = 1000;
+    std::cout << "On time: " << onRate << std::endl;
+    std::cout << "Mean inter-arrival time: " << 1/lambda - onRate << std::endl;
 
-uint32_t cnt_packets = 0;
+    // Set up Poisson traffic
+    Ptr<ExponentialRandomVariable> interArrival = CreateObject<ExponentialRandomVariable> ();
+    interArrival->SetAttribute ("Mean", DoubleValue (1/lambda - onRate)); // Set the mean inter-arrival time (lambda)
 
-void TraceCwnd(uint32_t node, uint32_t cwndWindow, Callback <void, uint32_t, uint32_t> CwndTrace){
-    Config::ConnectWithoutContext("/NodeList/" + std::to_string(node) + "/$ns3::TcpL4Protocol/SocketList/" + std::to_string(cwndWindow) + "/CongestionWindow", CwndTrace);
-}
+    OnOffHelper onoff ("ns3::UdpSocketFactory", Address (InetSocketAddress (destAddress, 9)));
+    onoff.SetAttribute ("DataRate", StringValue (flowDataRateStr));
+    onoff.SetAttribute ("OnTime", PointerValue (onTime));
+    onoff.SetAttribute ("OffTime", PointerValue (interArrival));
+    onoff.SetAttribute("PacketSize", UintegerValue(maxPacketSize));
 
-static void CwndChange(uint32_t oldCwnd, uint32_t newCwnd){
-  std::ofstream fPlotQueue(tracesPath + "cwnd.txt", std::ios::out | std::ios::app);
-  fPlotQueue << Simulator::Now().GetSeconds() << " " << newCwnd / segmentSize << " " << newCwnd << std::endl;
-  fPlotQueue.close();
-}
-
-void InstallBulkSend(Ptr<Node> node, Ipv4Address address, uint16_t port, std::string socketFactory,
-  uint32_t nodeId, uint32_t cwndWindow, Callback <void, uint32_t, uint32_t> CwndTrace, uint32_t maxBytesToSend, Time startTime){
-    BulkSendHelper source(socketFactory, InetSocketAddress(address, port));
-    source.SetAttribute("MaxBytes", UintegerValue(maxBytesToSend));
-    ApplicationContainer sourceApps = source.Install(node);
-    sourceApps.Start(startTime);
-    sourceApps.Stop (stopTimeUDP);
-        /* Trace the CWND */
-    if(storeTraces == false){
-        Simulator::Schedule(startTime + Seconds(0.001), &TraceCwnd, nodeId, cwndWindow, CwndTrace);
-    }
-}
-
-void InstallPacketSink(Ptr<Node> node, uint16_t port, std::string socketFactory, Time startTime, Time stopTime){
-    PacketSinkHelper sink (socketFactory, InetSocketAddress(Ipv4Address::GetAny(), port));
-    ApplicationContainer sinkApps = sink.Install(node);
-    sinker = StaticCast<PacketSink>(sinkApps.Get(0));
-    sinkApps.Start(startTime);
-    sinkApps.Stop(stopTime);
-}
-
-void RxDrop(Ptr<const Packet> packet) {
-    // Print Source and Destination IP Addresses
-    /* 
-     * Need to copy packet since headers need to be removed
-     * to be inspected. Alternatively, remove the headers,
-     * and add them back.
-     */
-    Ptr<Packet> copy = packet->Copy();
-
-    // Headers must be removed in the order they're present.
-    PppHeader pppHeader;
-    copy->RemoveHeader(pppHeader);
-    Ipv4Header ipHeader;
-    copy->RemoveHeader(ipHeader);
-
-    std::cout << "Source IP: ";
-    ipHeader.GetSource().Print(std::cout);
-    std::cout << std::endl;
-
-    std::cout << "Destination IP: ";
-    ipHeader.GetDestination().Print(std::cout);
-    std::cout << std::endl;
-}
-
-void ExaminePacket(Ptr< const Packet > packet){
-    // Todo: repurpose for UDP packets
-    // Extract UDP Header from the packet
-    TcpHeader * tcpHeader = (TcpHeader *) packet->extractTcpHeader();
-    uint32_t payloadSize = packet->GetPayloadSize();
-    int dPort = int(tcpHeader->GetDestinationPort());
-
-    // Extract the SEQ and ACK numbers
-    uint32_t seq = tcpHeader->GetSequenceNumber().GetValue();
-    uint32_t ack = tcpHeader->GetAckNumber().GetValue();
-
-    std::cout<<"Time: "<< Simulator::Now() << "[UDP PACKET] [SEQ: " << seq << "] [ACK: " << ack << "] [Payload Length: " << payloadSize << "] [Dest port:" << dPort << "] " << std::endl;
+    // Install the Poisson traffic source on src
+    ApplicationContainer apps = onoff.Install (src);
+    apps.Start (Seconds (0.2));
+    apps.Stop (stopTimeUDP);
 }
 
 int main (int argc, char *argv[]){
     // Command line arguments
     CommandLine cmd;
-    cmd.AddValue("bytesToSend", "Number of bytes to send", bytes_to_send);
+    cmd.AddValue("seed", "The random seed", seed);
     cmd.AddValue("delaySerialization", "Delay for serialization for all links", delay_serialization);
     cmd.AddValue("delayAccess", "Delay time for all links", delay_access);
+    cmd.AddValue("lambda1", "Parameter for Poisson process traffic, average number of packets a second", lambda_1);
+    cmd.AddValue("lambda2", "Parameter for Poisson process traffic, average number of packets a second", lambda_2);
     cmd.Parse(argc, argv);
+
+    RngSeedManager::SetSeed(seed);
 
     // Initialise nodes
     NodeContainer leftNodes, rightNodes, routers;
@@ -189,22 +140,15 @@ int main (int argc, char *argv[]){
     Ipv4InterfaceContainer routerToRight1IPAddress = ipAddresses.Assign(routerToRight1);
     ipAddresses.NewNetwork();
     Ipv4InterfaceContainer routerToRight2IPAddress = ipAddresses.Assign(routerToRight2);
-    ipAddresses.NewNetwork();
 
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
     /* Packet Printing is mandatory for the Packet Drop Test */
     Packet::EnablePrinting();
 
-    uint16_t server_port = 50000;
-    /* Install packet sink at receiver side */
-    InstallPacketSink(rightNodes.Get(0), server_port, socketFactory, Seconds(0.01), stopTimeSimulation);
-    InstallPacketSink(rightNodes.Get(1), server_port, socketFactory, Seconds(0.01), stopTimeSimulation);
+    InstallSourceSink(leftNodes.Get(0), routerToRight1IPAddress.GetAddress(1), lambda_1);
+    InstallSourceSink(leftNodes.Get(0), routerToRight2IPAddress.GetAddress(1), lambda_1);
 
-    /* Install BulkSend application */
-    InstallBulkSend(leftNodes.Get(0), routerToRight1IPAddress.GetAddress(1), server_port, socketFactory, 2, 0, MakeCallback(&CwndChange), bytes_to_send, Seconds(0.2));
-    InstallBulkSend(leftNodes.Get(0), routerToRight2IPAddress.GetAddress(1), server_port, socketFactory, 2, 0, MakeCallback(&CwndChange), bytes_to_send, Seconds(0.2));
-    
     /* Trace ASCII */
     pointToPointRouter.EnableAsciiAll(tracesPath);
 
